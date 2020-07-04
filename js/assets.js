a463f2fe7fb895295409efeaf33d6c2f60db0678
creativenerds <>
2020-07-04 15:34:06 +0100

    First project commit

---
+++ js/assets.js
@@ -0,0 +1,324 @@
+var assets = [
+
+    {
+      "order": 0,
+      "name": "icon-cn-alarm--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-battery-2--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-battery-3--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-battery-full--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-bulb--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-business--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-business--icon-1"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-calender--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-camera--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-cart--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-cart--icon-1"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-circle--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-clock--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-cross--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-cross--square--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-cut--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-disability--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-film--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-folded--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-forward--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-forward-square--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-headphone--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-headphone-plug--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-headset--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-heart--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-heart-minus--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-heart-plus--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-left-arrow-bubble"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-location--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-lock--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-menu--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-menu-outline--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn--message-block--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-message-dots--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-message--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-message--outline"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-minus--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-minus-square--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-mobile--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-mobile--icon-1"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-music--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-mute--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-mute--icon-1"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-off--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-old-film--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn--pause--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-pause-square--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-people--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-photo--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-play--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-play-square--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-plus--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-plus-square--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-power--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-profile--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-redo--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-repeat--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-rewind--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-rewind-square--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-right-arrow-bubble--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-search--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-search-minus-icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-search-plus--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-setting-cogs--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-setting--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-setting--icon-1"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-sound-down--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-sound-up--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-star--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-star-minus--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-star-outline--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-star-plus--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-stop--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-stop-square--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-tag--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-telephone--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-tick--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-tick-square--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-video--icon"
+    },
+    {
+      "order": 0,
+      "name": "icon-cn-wfi--icon"
+    }
+
+]
